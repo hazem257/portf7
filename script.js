21e46484978697dcfaa33ecfe77396c2c0665f6b
‪Hazem Gmall‬‏ <161766768+hazem257@users.noreply.github.com>
2025-01-27 20:54:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ let eff = new Typed(".typedText",{
   backSpeed : 80,
   backDelay : 2000
 })
+/*start dark mode */
+let cha = document.getElementById("mylink");
+let dar0 = document.getElementById("dar");
+let origin = 'style.css';
+let newHref = 'dark.css';
+let isorigin = true ;
+const image = document.getElementById('my-image');
 
+dar0.addEventListener('click',()=>{
+  if(isorigin){
+    cha.setAttribute('href',newHref);
+    
+  image.src = 'icons8-sun.svg';
+  }else{
+    cha.setAttribute('href',origin)
+  image.src = 'icons8-moon-48.png';
+  }
+  isorigin=!isorigin
+})
+
+
+/*DARK MODE */
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
